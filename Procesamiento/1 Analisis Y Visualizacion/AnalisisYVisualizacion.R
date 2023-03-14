8eea7b1e904f98aa88ab4e8e0a641eba38045992
SDB33 <salvadordb310@gmail.com>
2023-03-14 21:42:13 +0100

    Graficos

--- Procesamiento/1 Analisis Y Visualizacion/AnalisisYVisualizacion.R
+++ Procesamiento/1 Analisis Y Visualizacion/AnalisisYVisualizacion.R
@@ -12,7 +12,11 @@ auxi <- auxi[order(auxi,decreasing = TRUE)]
 
 ggplot(as.data.frame(auxi), aes(x=Var1, y = Freq)) + geom_bar(stat="identity")
 
-#SuperJueves
+ggplot(as.data.frame(auxi), aes(x="", y=Freq, fill=Var1)) +
+  geom_bar(stat="identity", width=1) +
+  coord_polar("y", start=0)
+
+#¿Hay diferencias entre lo que estudian los hombres y las mujeres?
 
 auxi <- Empleo %>%
   select(Gender,Studies.grouped.by.field) %>%
@@ -34,22 +38,32 @@ auxi <- inner_join(auxi, (Empleo %>%
                     summarise(Mujeres=n()) %>%
                     arrange(Mujeres))
                           , by = "Studies.grouped.by.field")
+for (x in c(1:nrow(auxi))) {
+  auxi[nrow(auxi) + 1,] <- auxi %>% slice(x)
+}
 
+auxi <- auxi %>%
+  mutate(Sexo = if_else(row_number()<=7,"Hombre", "Mujer"))
+
+for (x in c(((nrow(auxi)/2)+1):nrow(auxi)  )   ) {
+  print(x)
+  auxi$Hombres[x] <- auxi$Mujeres[ (x - (nrow(auxi)/2) )    ]
+}
+
+auxi <- auxi %>%
+  select(-3) %>%
+  rename( "Contar" = Hombres)
 
 #No hay mucha disparidad entre elecciones de campo de estudios por género
 #hay que decir también que estos  datos son "de juguete" pueden estar más que
 #perfectamente sesgados o simplemente ser tan pequeños que no muestran nada
 
 
+ggplot(auxi, aes(x=unlist(auxi$Studies.grouped.by.field), y=Contar, fill = Sexo )) +
+  geom_bar( stat='identity',position='dodge')
 
-ggplot(auxi, aes(x = unlist(Studies.grouped.by.field), y = Hombres )) + 
-  geom_col()  + 
-  coord_flip()
 
-ggplot(auxi, aes(x = unlist(Studies.grouped.by.field), y = -Mujeres)) + 
-  geom_col()  + 
-  coord_flip()
-  
+
 
 ####Marital Status and interested in freelance
 
@@ -108,3 +122,10 @@ ggplot(auxi, aes(x = unlist(Age.group), y = numero, colour = Interested.in.freel
 #sorprendentemente tienen el porcentaje más alto de personas que quieren ser freelancers con casi un
 # 60%
 #reitero que todo esto considerando que son datos de juguete
+
+
+
+
+##
+
+
