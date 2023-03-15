f3bde3919c6675337f4ea7b9bae2b89f181cd9d1
SDB33 <salvadordb310@gmail.com>
2023-03-15 18:53:14 +0100

    + Visualización

--- Procesamiento/1 Analisis Y Visualizacion/AnalisisYVisualizacion.R
+++ Procesamiento/1 Analisis Y Visualizacion/AnalisisYVisualizacion.R
@@ -10,11 +10,23 @@ library(tidyr)
 auxi <- table(unlist(Empleo$Job.finder))
 auxi <- auxi[order(auxi,decreasing = TRUE)]
 
-ggplot(as.data.frame(auxi), aes(x=Var1, y = Freq)) + geom_bar(stat="identity")
+ggplot(as.data.frame(auxi), aes(x="", y=Freq, fill=Var1)) +
+  geom_bar(stat="identity", width=1) +
+  coord_polar("y", start=0) + 
+  labs(fill = "Buscadores", x = " ", y = " ")
+
+#¿Cuáles son las competencias que más tienen las personas encuestadas?
+
+auxi <- table(unlist(Empleo$Skills.grouped))
+auxi <- auxi[order(auxi,decreasing = TRUE)]
+
 
 ggplot(as.data.frame(auxi), aes(x="", y=Freq, fill=Var1)) +
   geom_bar(stat="identity", width=1) +
-  coord_polar("y", start=0)
+  coord_polar("y", start=0) + 
+  labs(fill = "Competencia", x = " ", y = " ")
+
+
 
 #¿Hay diferencias entre lo que estudian los hombres y las mujeres?
 
@@ -54,13 +66,14 @@ auxi <- auxi %>%
   select(-3) %>%
   rename( "Contar" = Hombres)
 
+ggplot(auxi, aes(x=unlist(Studies.grouped.by.field), y=Contar, fill = Sexo )) +
+  geom_bar( stat='identity',position='dodge') +
+  labs( fill = "Género", x = "Estudios por campo", y = "Número")
+
+
 #No hay mucha disparidad entre elecciones de campo de estudios por género
 #hay que decir también que estos  datos son "de juguete" pueden estar más que
 #perfectamente sesgados o simplemente ser tan pequeños que no muestran nada
-
-
-ggplot(auxi, aes(x=unlist(auxi$Studies.grouped.by.field), y=Contar, fill = Sexo )) +
-  geom_bar( stat='identity',position='dodge')
 
 
 
@@ -84,7 +97,8 @@ auxi <- auxi[-4,]
 
 
 ggplot(auxi, aes(x = unlist(Marital.status), y = numero, colour = Interested.in.freelance)) + 
-   geom_point()
+   geom_point() + 
+  labs( color = "Interesado en freelance", x = "Estado Civil", y = "Número")
 
 #Tampoco hay diferencias muy reseñables aunque sí que es cierto que, ligeramente,
 #hay mayor porcentaje de personas casadas que están seguras de que no quieren
@@ -121,11 +135,64 @@ ggplot(auxi, aes(x = unlist(Age.group), y = numero, colour = Interested.in.freel
 #Los de 25-30 años tienen tanto el mísmo interés en serlo como en no serlo y los de 31-40
 #sorprendentemente tienen el porcentaje más alto de personas que quieren ser freelancers con casi un
 # 60%
-#reitero que todo esto considerando que son datos de juguete
+
+
+
+## Vamos a ver "Cuánta gente que trabaja está contenta con su trabajo"
+
+auxi <- Empleo %>%
+  select(Currently.employed,Applying.4.job) %>%
+  group_by(Currently.employed,Applying.4.job) %>%
+  summarise( numero = n()) 
+
+
+auxi[[3]][which(auxi$Currently.employed == "No")] = 
+  (auxi[[3]][which(auxi$Currently.employed == "No")])/sum(auxi[[3]][which(auxi$Currently.employed == "No")])
+
+auxi[[3]][which(auxi$Currently.employed == "Yes")] = 
+  (auxi[[3]][which(auxi$Currently.employed == "Yes")])/sum(auxi[[3]][which(auxi$Currently.employed == "Yes")])
+
+ggplot(auxi, aes(x = unlist(Currently.employed), y = numero, fill = Applying.4.job)) + 
+  geom_bar( stat='identity')  + 
+  labs( fill = "Buscando Trabajo", x = "Trabajando", y = "Número")
+
+
+# Empezando por abajo (Yes,No,Casually) es normal que gente que no tenga trabajo busque
+# más que los que si tienen y viceversa con "No"
+# Pero vemos que hay un alto porcentaje de personas que no tienen trabajo y casualmente buscan
+# ¿Estarán estudiando?
+
+auxi <- Empleo %>%
+  select(Currently.employed,Applying.4.job, Still.Studying) %>%
+  group_by(Currently.employed,Applying.4.job,Still.Studying) %>%
+  summarise( numero = n()) 
+
+auxi[[4]][which(auxi$Currently.employed == "No" & auxi$Still.Studying == "No")] = 
+  (auxi[[4]][which(auxi$Currently.employed == "No" & auxi$Still.Studying == "No")])/sum(auxi[[4]][which(auxi$Currently.employed == "No" & auxi$Still.Studying == "No")])
+
+auxi[[4]][which(auxi$Currently.employed == "No" & auxi$Still.Studying == "Yes")] = 
+  (auxi[[4]][which(auxi$Currently.employed == "No" & auxi$Still.Studying == "Yes")])/sum(auxi[[4]][which(auxi$Currently.employed == "No" & auxi$Still.Studying == "Yes")])
 
 
 
 
-##
+auxi[[4]][which(auxi$Currently.employed == "Yes" & auxi$Still.Studying == "No")] = 
+  (auxi[[4]][which(auxi$Currently.employed == "Yes" & auxi$Still.Studying == "No")])/sum(auxi[[4]][which(auxi$Currently.employed == "Yes" & auxi$Still.Studying == "No")])
+
+auxi[[4]][which(auxi$Currently.employed == "Yes" & auxi$Still.Studying == "Yes")] = 
+  (auxi[[4]][which(auxi$Currently.employed == "Yes" & auxi$Still.Studying == "Yes")])/sum(auxi[[4]][which(auxi$Currently.employed == "Yes" & auxi$Still.Studying == "Yes")])
 
 
+ggplot(auxi, aes(x = unlist(Currently.employed), y = numero, fill = Applying.4.job)) +
+  geom_bar( stat='identity')  +
+  facet_grid(.~Still.Studying) +
+  labs( fill = "Buscando Trabajo", x = "Trabajando", y = "Número")
+
+# Vemos que debe haber otro factor porque, por ejemplo, las personas que no
+# trabajan y no estudian, ¿Cómo es posible que estén buscando empleo de forma
+# "Casually"
+# Luego, No trabajando y Sí estudiando me que los datos son "normales"
+# Sí trabajando y no estudiando es lo que no hemos podido determinar pero 
+# podría ser que no estuvieran conformes con su trabajo
+# Por último, Sí trabajando y Sí estudiando se podría explicar debido a que
+# estén de prácticas empresariales, por ejemplo.
